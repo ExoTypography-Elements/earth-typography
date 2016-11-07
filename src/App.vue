4a79b85e0fe1a7c46ee03d9fdbee13a2000f7553
Webmaker <fitzflood@gmail.com>
2016-11-07 14:43:09 -0600

    general additions and updates

--- src/App.vue
+++ src/App.vue
@@ -26,21 +26,21 @@
       <div class="page p-flex mx-auto">
         
         <nav role="navigation" style="max-width: 960px;" class=" mx-auto">
-          <ul>
-            <li>
+          <ul  class="row">
+            <li class="col">
               <a href="#text">Text</a>
-              <ul class="row">
-                <li class="col"><a href="#text__headings">Headings</a></li>
-                <li class="col"><a href="#text__paragraphs">Paragraphs</a></li>
-                <li class="col"><a href="#text__blockquotes">Blockquotes</a></li>
-                <li class="col"><a href="#text__lists">Lists</a></li>
+              <ul>
+                <li ><a href="#text__headings">Headings</a></li>
+                <li><a href="#text__paragraphs">Paragraphs</a></li>
+                <li><a href="#text__blockquotes">Blockquotes</a></li>
+                <li><a href="#text__lists">Lists</a></li>
                 <li><a href="#text__hr">Horizontal rules</a></li>
                 <li><a href="#text__tables">Tabular data</a></li>
                 <li><a href="#text__code">Code</a></li>
                 <li><a href="#text__inline">Inline elements</a></li>
               </ul>
             </li>
-            <li>
+            <li  class="col">
               <a href="#embedded">Embedded content</a>
               <ul>
                 <li><a href="#embedded__images">Images</a></li>
@@ -53,7 +53,7 @@
                 <li><a href="#embedded__iframe">IFrames</a></li>
               </ul>
             </li>
-            <li>
+            <li  class="col">
               <a href="#forms">Form elements</a>
               <ul>
                 <li><a href="#forms__input">Input fields</a></li>
